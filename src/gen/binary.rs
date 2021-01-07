3584bac1a1fbe6b79b03d5813dc6a75fef1ab6e7
Kyuuhachi <caagr98@gmail.com>
2021-01-07 16:38:59 +0100

    Make binary support all four directions

--- src/gen/binary.rs
+++ src/gen/binary.rs
@@ -5,9 +5,11 @@ pub fn generate(w: usize, h: usize) -> Maze {
 	let mut rng = rand::thread_rng();
 	let mut maze = Maze::new(w, h, true);
 
-	for x in 0..w-1 {
-		for y in 0..h-1 {
-			let dir = if rng.gen() {Dir::Right} else {Dir::Down};
+	let (hdir, hmin, hmax) = if rng.gen() {(Dir::Right, 0, w-1)} else {(Dir::Left, 1, w)};
+	let (vdir, vmin, vmax) = if rng.gen() {(Dir::Down,  0, h-1)} else {(Dir::Up,   1, h)};
+	for x in hmin..hmax {
+		for y in vmin..vmax {
+			let dir = if rng.gen() {vdir} else {hdir};
 			maze[(dir, (x, y))] = false;
 		}
 	}
