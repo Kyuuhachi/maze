ad16396b30a4f01c5d7fd98af3a297850f371d62
Kyuuhachi <caagr98@gmail.com>
2021-01-10 21:07:32 +0100

    Use closure properly

--- src/gen/kruskal.rs
+++ src/gen/kruskal.rs
@@ -18,14 +18,11 @@ impl Generator for Kruskal {
 		let mut maze = Maze::new(size, false);
 		let mut poss = Vec::new();
 		let mut uf: InPlaceUnificationTable<Key> = InPlaceUnificationTable::new();
-		let mut keys = Array2::from_shape_simple_fn(size, || Key(0));
-		for x in 0..maze.w() {
-			for y in 0..maze.h() {
-				keys[(x, y)] = uf.new_key(());
-				poss.push((Dir::Down, (x, y)));
-				poss.push((Dir::Right, (x, y)));
-			}
-		}
+		let keys = Array2::from_shape_fn(size, |pos| {
+			poss.push((Dir::Down, pos));
+			poss.push((Dir::Right, pos));
+			uf.new_key(())
+		});
 		poss.shuffle(&mut rng);
 		for &(dir, pos) in &poss {
 			if let Some(pos2) = maze.shift(dir, pos) {
